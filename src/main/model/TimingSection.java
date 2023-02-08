6747aa7ed5a5867c207793a71d3adfdd927e4449
Eitan <ekling01@student.ubc.ca>
2023-02-07 21:27:30 -0800

    New Song class Getters and setters for everything Implementation of find in Song

--- src/main/model/TimingSection.java
+++ src/main/model/TimingSection.java
@@ -1,15 +1,61 @@
 package model;
 
+import java.sql.Time;
+
+// TimingSection represents a section with an initial timestamp, bpm, and time signature (top / bot)
 public class TimingSection {
 
-    private int timeStamp;
+    private int timestamp;
     private double bpm;
     private TimeSignature timesig;
 
+
     public TimingSection(int time, double bpm, TimeSignature ts) {
-        this.timeStamp = time;
+        this.timestamp = time;
         this.bpm = bpm;
-        this.timesig = ts;
+        timesig = ts;
+    }
+
+
+    public void setTime(int time) {
+        this.timestamp = time;
+    }
+
+
+    public void setBPM(double bpm) {
+        this.bpm = bpm;
+    }
+
+
+
+
+    // REQUIRES: top > 0 and bot > 0
+    // MODIFIES: this
+    // EFFECTS: change time signature to be (top / bot)
+    public void setTimeSig(int top, int bot) {
+        timesig = new TimeSignature(top, bot);
+    }
+
+
+    public int getTime() {
+        return timestamp;
+    }
+
+
+    public double getBPM() {
+        return bpm;
+    }
+
+    public TimeSignature getTimesig() {
+        return timesig;
+    }
+
+    // TODO
+    // REQUIRES: bpm > 0
+    // MODIFIES:
+    // EFFECTS: returns how long a single beat is in milliseconds
+    public double beatLength() {
+        return 60000 / bpm;
     }
 
 
